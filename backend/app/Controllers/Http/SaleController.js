67d75f85f9a5849ad7df9af8ed6c671e7ee1f05c
brunocardosof <bruno.cardosof@gmail.com>
2021-02-02 00:58:50 -0300

    backend(sales) make sale with check stock balance and subtract stock balance methods

--- backend/app/Controllers/Http/SaleController.js
+++ backend/app/Controllers/Http/SaleController.js
@@ -11,18 +11,48 @@ const Database = use('Database');
  * Resourceful controller for interacting with sales
  */
 class SaleController {
+
+  stock = 0;
+  product_id = 0;
+  product_quantity = 0;
+  product_price = 0;
+
   async sale({ request, response }) {
     const dataForSale = request.only(['date', 'user_id']);
     const propertiesForSalesProduct = request.post();
-    await Sale.create(dataForSale)
-      .then((result) => {
+
+    this.product_id = propertiesForSalesProduct.product_id;
+    this.product_quantity = propertiesForSalesProduct.quantity;
+
+    //Check if product have on stock
+    await this.checkStockBalance();
+
+    if (this.stock < 1) {
+      return response.status(409).json({
+        message: `stock 0 for this product with id ${this.product_id}`
+      });
+    }
+    if (this.product_quantity > this.stock) {
+      return response.status(409).json({ message: `the quantity of sale (${this.product_quantity}) is greater than have on stock balance (${this.stock})` });
+    }
+
+    Sale.create(dataForSale)
+      .then(async (result) => {
         const dataForSalesProduct = {
           sales_id: result.id,
           product_id: propertiesForSalesProduct.product_id,
           quantity: propertiesForSalesProduct.quantity,
-          total_price: propertiesForSalesProduct.total_price,
+          total_price: this.price * propertiesForSalesProduct.quantity,
         };
-        this.salesProduct(dataForSalesProduct);
+        // Add de sales informations on sales_product table
+        const salesProduct = await this.salesProduct(dataForSalesProduct);
+        if (salesProduct) {
+          // if have stock balance and sales is saved on sales_product information,
+          // the stock balance of the product is subtracted
+          const result = this.subtractStockBalance();
+          return response.json(result);
+
+        }
       })
       .catch((error) => {
         return response.status(400).json(error);
@@ -40,6 +70,24 @@ class SaleController {
         created_at: new Date().toLocaleString(),
         updated_at: new Date().toLocaleString()
       });
+  }
+
+  async checkStockBalance() {
+    const stock_balance = await Database
+      .from('products')
+      .select('stock_balance', 'price')
+      .where('id', this.product_id);
+    this.stock = stock_balance[0].stock_balance;
+    this.price = stock_balance[0].price;
+  }
+
+  async subtractStockBalance() {
+    const stockForSave = this.stock - this.product_quantity;
+    return await Database
+      .table('products')
+      .where('id', this.product_id)
+      .update('stock_balance', stockForSave);
+
   }
 
 }
