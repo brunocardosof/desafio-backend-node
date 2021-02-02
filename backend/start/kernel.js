617aa5d8eb868f87e633feac2d49488b4217f3a9
brunocardosof <bruno.cardosof@gmail.com>
2021-02-02 15:07:26 -0300

    backend(authorization) add AdminAuth middleware with validation if user role is equal admin

--- backend/start/kernel.js
+++ backend/start/kernel.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/framework/src/Server')} */
-const Server = use('Server')
+const Server = use('Server');
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +15,7 @@ const Server = use('Server')
 const globalMiddleware = [
   'Adonis/Middleware/BodyParser',
   'App/Middleware/ConvertEmptyStringsToNull'
-]
+];
 
 /*
 |--------------------------------------------------------------------------
@@ -35,9 +35,10 @@ const globalMiddleware = [
 |
 */
 const namedMiddleware = {
+  adminAuth: 'App/Middleware/AdminAuth',
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly'
-}
+  // guest: 'Adonis/Middleware/AllowGuestOnly',
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -52,9 +53,9 @@ const namedMiddleware = {
 const serverMiddleware = [
   // 'Adonis/Middleware/Static',
   'Adonis/Middleware/Cors'
-]
+];
 
 Server
   .registerGlobal(globalMiddleware)
   .registerNamed(namedMiddleware)
-  .use(serverMiddleware)
+  .use(serverMiddleware);
