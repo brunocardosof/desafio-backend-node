a6a7f8cfa8869846797a63b7e5ec7cc668b7fb33
brunocardosof <bruno.cardosof@gmail.com>
2021-02-03 19:37:27 -0300

    add user seed

---
+++ backend/database/seeds/UserSeeder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/*
+|--------------------------------------------------------------------------
+| UserSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Hash = use('Hash');
+const User = use("App/Models/User");
+
+class UserSeeder {
+  async run() {
+    await User.create({
+      "username": "Admin",
+      "email": "admin@hotmail.com",
+      "password": '123456',
+      "role": "client"
+    });
+    await User.create({
+      "username": "client",
+      "email": "client@hotmail.com",
+      "password": '123456',
+      "role": "admin"
+    });
+  }
+}
+
+module.exports = UserSeeder;
